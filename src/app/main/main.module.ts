370d999793da01f002e1e8f34ac8a792e3006f52
Sianna Puente <siannaapuentec@gmail.com>
2018-07-01 15:06:35 -0500

    migración a angular 6

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import {MainRoutingModule} from './main-routing.module';
-import {DropdownModule, InputTextModule, ListboxModule, TabViewModule} from 'primeng/primeng';
+import {DropdownModule, InputTextModule, ListboxModule, MultiSelectModule, TabViewModule} from 'primeng/primeng';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
@@ -14,7 +14,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     DropdownModule,
     FormsModule,
     ReactiveFormsModule,
-    ListboxModule
+    ListboxModule,
+    MultiSelectModule
   ],
   declarations: [MainComponent]
 })
